da13582510b8f72b49b9e23affccd71ceff6c3d1
Salomé Tredici <salome.tredici@outlook.com>
2026-04-26 21:11:44 -0300

    auctionpage update

--- src/app/pages/auctions/CreateAuctionPage.tsx
+++ src/app/pages/auctions/CreateAuctionPage.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { CollectionCard } from '../../interfaces/cards/CollectionCard';
 import { getUserCollection } from '../../api/UsersService';
 import { createAuction } from '../../api/AuctionsService';
-import { theme } from '../../styles/theme';
+import { theme } from '../../styles/theme'; 
 import { AUCTION_DURATION_MIN, AUCTION_DURATION_MAX } from '../../constants/auctions';
 import { formatDuration } from '../../utils/utils';
 import { PageContainer, Header, BackButton, Title, Card, Field, Label, Hint, Select,
